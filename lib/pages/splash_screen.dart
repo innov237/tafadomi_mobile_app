bed64731ea4646feab3933a4de9a09e551b32f5b
innov237 <innov237@gmail.com>
2021-01-29 10:12:33 +0100

    connection with dio

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
-import 'package:tafadomi/widgets/prestataire.dart';
 import 'package:tafadomi/pages/home_page.dart';
 
 class Splash extends StatefulWidget {
